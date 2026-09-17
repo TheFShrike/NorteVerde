7d911a27be00de923df48a2f250334ba06732187
TheFShrike <106623407+TheFShrike@users.noreply.github.com>
2026-09-17 17:11:52 +0000

    Update script.js

--- script.js
+++ script.js
@@ -31,28 +31,11 @@ async function obtenerClima() {
 
 obtenerClima();
 
+//Menú de botones
 
-// --- SECCIÓN DEL FONDO DINÁMICO ---
+const btnMenu = document.getElementById('btnMenu');
+        const nav = document.querySelector('nav');
 
-// 1. Configuración de la ruta y las imágenes
-const rutaCarpeta = './Portada/'; 
-const imagenes = ['foto1.jpg', 'foto2.jpg', 'foto3.jpg'];
-
-let indiceActual = 0;
-const contenedorFondo = document.getElementById('fondoimg');
-
-// 2. Función que cambia la imagen de fondo
-function cambiarFondo() {
-    if (!contenedorFondo || imagenes.length === 0) return;
-
-    // CORREGIDO: Se cambió 'Portada' por 'rutaCarpeta'
-    const urlImagen = `url('${rutaCarpeta}${imagenes[indiceActual]}')`;
-    contenedorFondo.style.backgroundImage = urlImagen;
-
-    // Avanza al siguiente índice (y vuelve a cero al llegar al final)
-    indiceActual = (indiceActual + 1) % imagenes.length;
-}
-
-// 3. Iniciar el ciclo del fondo
-cambiarFondo(); // Muestra la primera foto de inmediato
-setInterval(cambiarFondo, 5000); // Cambia cada 5 segundos
+        btnMenu.addEventListener('click', () => {
+            nav.classList.toggle('open');
+        });
