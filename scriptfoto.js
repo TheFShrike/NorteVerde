91a5986b6eb1ba24dae138478296453c3ffb7d1d
TheFShrike <106623407+TheFShrike@users.noreply.github.com>
2026-09-17 10:38:14 -0400

    Update scriptfoto.js

--- scriptfoto.js
+++ scriptfoto.js
@@ -1,16 +1,16 @@
 // 1. Configuración de la ruta y las imágenes
 const rutaCarpeta = './Portada/'; 
-const imagenes = ['foto1.jpg', 'foto2.jpg', 'foto3.jpg', 'foto4.jpg'];
+const imagenes = ['foto1.jpg', 'foto2.jpg', 'foto3.jpg'];
 
 let indiceActual = 0;
-const contenedorFondo = document.getElementById('fondo');
+const contenedorFondo = document.getElementById('fondoimg');
 
 // 2. Función que cambia la imagen de fondo
 function cambiarFondo() {
     if (imagenes.length === 0) return;
 
     // Construye la ruta completa
-    const urlImagen = `url('${rutaCarpeta}${imagenes[indiceActual]}')`;
+    const urlImagen = `url('${Portada}${imagenes[indiceActual]}')`;
     contenedorFondo.style.backgroundImage = urlImagen;
 
     // Avanza al siguiente índice (y vuelve a cero al llegar al final)
